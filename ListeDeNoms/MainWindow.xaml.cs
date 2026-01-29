82859c07ae20cbcc99fcdcbbb23f936dd8856512
Nathan Thériault <2443432@cegeprdl.ca>
2026-01-29 17:28:35 -0500

    V1.0 | 3/4 Fait

--- ListeDeNoms/MainWindow.xaml.cs
+++ ListeDeNoms/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ListeDeNoms
 {
@@ -7,14 +9,23 @@ namespace ListeDeNoms
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        int tbcompteur = 0;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        void OnEdtNomKeyDown(object sender, EventArgs e)
+        void OnEdtNomKeyDown(object sender, KeyEventArgs e) // KEY !!!
         {
             // Comment on ajoute un évènement en xaml?
+
+            //OnBtnAddNameClick()
+            if (e.Key == Key.Enter)
+            {
+                AddName(edtName.Text);
+            }
+
         }
 
         void OnBtnAddNameClick(object sender, EventArgs e)
@@ -25,7 +36,21 @@ namespace ListeDeNoms
         // Avoir un message box qui montre le nom
         void AddName(string name)
         {
-            MessageBox.Show(name);
+            var tbNom = new TextBlock();
+            tbNom.Margin = new Thickness(375, tbcompteur * 15, 0, 0); // +15 // 2eme // l,t,r,b \\
+            gridNoms.Children.Add(tbNom);
+            tbNom.Text = name;
+            tbcompteur++;
+
+            if (tbcompteur >= 10)
+            {
+                OnBtnAddName.IsEnabled = false;
+                MessageBox.Show("Vous avez assez ajouter de nom !!");
+
+
+            }
+            edtName.Clear();
+
 
         }
     }
