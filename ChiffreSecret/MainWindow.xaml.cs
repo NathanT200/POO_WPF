413828db2569305de17cfa4c95b0400f1e7b0f09
Nathan Thériault <2443432@cegeprdl.ca>
2026-01-30 10:03:37 -0500

    V1.1 | parti 4

--- ChiffreSecret/MainWindow.xaml.cs
+++ ChiffreSecret/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace ChiffreSecret
 {
@@ -14,9 +15,29 @@ namespace ChiffreSecret
             // Va voir le menu!
         }
 
+        void OnMenuNewGameClick(object sender, EventArgs e)
+        {
+
+
+        }
+
+
         private void PrepareButtons()
         {
             // Ici tu vas préparer tes boutons.
+
+
+            //btnNom.Width = Double.NaN;
+
+            for (int i = 1; i <= 20; i++)
+            {
+                var btnNom = new Button();
+                btnNom.Margin = new Thickness(0, i * 50, 0, 0); // +15 // 2eme // l,t,r,b \\
+                GridButtons.Children.Add(btnNom);
+                btnNom.Width = 50;
+                btnNom.Name = "btn" + i;
+                btnNom.Content = i;
+            } // marche pas 
         }
     }
 }
